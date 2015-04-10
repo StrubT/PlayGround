d98af7a16a991d18294a9a806caba2f9e0680dea
Thomas Reto Strub <ths@strubt.ch>
2015-04-10 18:45:44 +0200

    added ReferenceAndValueTypes

---
+++ CSharpConsole/ReferenceAndValueTypes.cs
@@ -0,0 +1,43 @@
+﻿using c = System.Console;
+
+namespace ThS.PlayGround.CSharpConsole {
+
+	public class ReferenceAndValueTypes : IRunnable {
+
+		public void run() {
+
+			var referenceType = new ReferenceType();
+			var referenceTypeByReference = new ReferenceType();
+			var valueType = new ValueType();
+			var valueTypeByReference = new ValueType();
+
+			this.changeReferenceType(referenceType);
+			this.changeReferenceTypeByReference(ref referenceTypeByReference);
+			this.changeValueType(valueType);
+			this.changeValueTypeByReference(ref valueTypeByReference);
+
+			c.WriteLine("reference type:                       {0}changed", !referenceType.changed ? "NOT " : string.Empty);
+			c.WriteLine("reference type (passed by reference): {0}changed", !referenceTypeByReference.changed ? "NOT " : string.Empty);
+			c.WriteLine("value type:                           {0}changed", !valueType.changed ? "NOT " : string.Empty);
+			c.WriteLine("value type (passed by reference):     {0}changed", !valueTypeByReference.changed ? "NOT " : string.Empty);
+		}
+
+		private void changeReferenceType(ReferenceType changeable) { changeable.changed = true; }
+
+		private void changeReferenceTypeByReference(ref ReferenceType changeable) { changeable.changed = true; }
+
+		private void changeValueType(ValueType changeable) { changeable.changed = true; }
+
+		private void changeValueTypeByReference(ref ValueType changeable) { changeable.changed = true; }
+	}
+
+	internal class ReferenceType {
+
+		public bool changed { get; set; }
+	}
+
+	internal struct ValueType {
+
+		public bool changed { get; set; }
+	}
+}
