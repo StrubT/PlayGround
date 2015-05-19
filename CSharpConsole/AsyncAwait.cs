d6e5a6a3bf25494a5b79cf3be394af79cfad9bf8
Thomas Reto Strub <ths@strubt.ch>
2015-05-19 23:11:04 +0200

    added AsyncAwait

---
+++ CSharpConsole/AsyncAwait.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using c = System.Console;
+
+namespace StrubT.PlayGround.CSharpConsole {
+
+	public class AsyncAwait : IRunnable {
+
+		public bool IsActive() { return false; }
+
+		public async void Run() {
+
+			c.WriteLine("enter something:");
+
+			var tsk = Task.Delay(5000);
+
+			string line;
+			while ((line = c.ReadLine()).Length > 0)
+				c.WriteLine(">{0}", string.Join(string.Empty, line.Reverse()));
+
+			await tsk;
+
+			c.WriteLine("finished!");
+		}
+	}
+}
