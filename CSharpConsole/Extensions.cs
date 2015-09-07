d6a6e73743ce0763d397aaacfed4412569b61ed1
Thomas Reto Strub <ths@strubt.ch>
2015-09-07 12:58:48 +0200

    print information about countries

---
+++ CSharpConsole/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StrubT.PlayGround.CSharpConsole {
+
+	internal static class Extensions {
+
+		public static T NullIf<T>(this T instance, Predicate<T> condition) where T : class => !condition(instance) ? instance : null;
+	}
+}
--- CSharpConsole/RestCountries.cs
+++ CSharpConsole/RestCountries.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using Newtonsoft.Json.Linq;
+using c = System.Console;
 
 namespace StrubT.PlayGround.CSharpConsole {
 
@@ -16,6 +17,7 @@ namespace StrubT.PlayGround.CSharpConsole {
 
 		public void Run() {
 
+			var col = c.ForegroundColor;
 			var key = File.ReadAllText("C:\\temp\\MashapeKey.txt");
 
 			using (var web = new WebClient()) {
@@ -32,11 +34,11 @@ namespace StrubT.PlayGround.CSharpConsole {
 																CodeAlpha3 = c.Value<string>("alpha3Code"),
 																NameEnglish = c.Value<string>("name"),
 																NameNative = c.Value<string>("nativeName"),
-																NamesAlternateRaw = c.Value<JArray>("altSpellings").Values<string>().ToList(),
+																NamesAlternativeRaw = c.Value<JArray>("altSpellings").Values<string>().ToList(),
 																NamesTranslatedRaw = c.Value<JObject>("translations").Properties().ToDictionary(t => t.Name, t => (string)t.Value),
-																Capital = c.Value<string>("capital"),
-																Demonym = c.Value<string>("demonym"),
-																Region = c.Value<string>("region"),
+																Capital = c.Value<string>("capital").NullIf(string.IsNullOrEmpty),
+																Demonym = c.Value<string>("demonym").NullIf(string.IsNullOrEmpty),
+																Region = c.Value<string>("region").NullIf(string.IsNullOrEmpty),
 																SubRegion = c.Value<string>("subregion"),
 																//Relevance = c.Value<string>("relevance"),
 																Population = c.Value<int?>("population"),
@@ -44,7 +46,7 @@ namespace StrubT.PlayGround.CSharpConsole {
 																Gini = c.Value<double?>("gini"),
 																Location = c.Value<JArray>("latlng")?.Count == 2 ? new GeoCoordinate(c.Value<JArray>("latlng").Value<double>(0), c.Value<JArray>("latlng").Value<double>(1)) : GeoCoordinate.Unknown,
 																SharesBorderWithCodesAlpha3 = (ICollection<string>)c.Value<JArray>("borders")?.Values<string>().ToList() ?? new string[] { },
-																SpokenLanguagesCodesAlpha2 = (ICollection<string>)c.Value<JArray>("languages")?.Values<string>().ToList() ?? new string[] { },
+																LanguagesCodesAlpha2 = (ICollection<string>)c.Value<JArray>("languages")?.Values<string>().ToList() ?? new string[] { },
 																TimeZonesCodes = (ICollection<string>)c.Value<JArray>("timezones")?.Values<string>().ToList() ?? new string[] { },
 																CallingCodes = (ICollection<string>)c.Value<JArray>("callingCodes")?.Values<string>().ToList() ?? new string[] { },
 																TopLevelDomains = (ICollection<string>)c.Value<JArray>("topLevelDomain")?.Values<string>().ToList() ?? new string[] { },
@@ -52,10 +54,73 @@ namespace StrubT.PlayGround.CSharpConsole {
 															})
 						Country.Lookup.Add(cty);
 
-				var ch = Country.Lookup.Single(c => c.CodeAlpha2 == "CH");
-				var gb = Country.Lookup.Single(c => c.CodeAlpha2 == "GB");
-				var us = Country.Lookup.Single(c => c.CodeAlpha2 == "US");
+				WriteRegionTree();
+				c.WriteLine();
+				c.WriteLine();
+
+				WriteCountry(Country.Lookup.Single(c => c.CodeAlpha2 == "CH"));
+				c.WriteLine();
+				WriteCountry(Country.Lookup.Single(c => c.CodeAlpha2 == "GB"));
+				c.WriteLine();
+				WriteCountry(Country.Lookup.Single(c => c.CodeAlpha2 == "US"));
+				c.WriteLine();
+				WriteCountry(Country.Lookup.Single(c => c.CodeAlpha2 == "AU"));
+				c.WriteLine();
+				WriteCountry(Country.Lookup.Single(c => c.CodeAlpha2 == "UM"));
 			}
+
+			c.ForegroundColor = col;
+		}
+
+		private void WriteRegionTree() {
+
+			c.ForegroundColor = ConsoleColor.Gray;
+			foreach (var reg in from c in Country.Lookup
+													where c.Region != null
+													group c by c.Region into r
+													orderby r.Key
+													select r) {
+				c.WriteLine($">{reg.Key}");
+
+				foreach (var sub in from c in reg
+														where c.SubRegion != null
+														group c by c.SubRegion into s
+														orderby s.Key
+														select s) {
+					c.WriteLine($"->{sub.Key}");
+
+					c.ForegroundColor = ConsoleColor.DarkGray;
+					c.WriteLine("-->{0}", string.Join(", ", from c in sub
+																									orderby c.NameEnglish
+																									select c.NameEnglish));
+					c.ForegroundColor = ConsoleColor.Gray;
+				}
+			}
+		}
+
+		private void WriteCountry(Country country) {
+
+			c.WriteLine($"*** {country.NameEnglish} ***");
+			c.WriteLine($"ISO codes: {country.CodeAlpha2} / {country.CodeAlpha3}");
+			c.WriteLine($"Native name: {country.NameNative}");
+			c.WriteLine($"Alternative name(s): {string.Join(" / ", country.NamesAlternative)}");
+			c.WriteLine($"Translated name(s): {string.Join(", ", country.NamesTranslated.Select(t => $"{t.Key}: {t.Value}"))}");
+			c.WriteLine($"Capital: {country.Capital ?? "n/a"}");
+			c.WriteLine($"Demonym: {country.Demonym ?? "n/a"}");
+			c.WriteLine($"Region: {country.Region ?? "n/a"}");
+			c.WriteLine($"Sub-region: {country.SubRegion ?? "n/a"}");
+			//c.WriteLine($"Relevance: {country.Relevance}");
+			c.WriteLine($"Population: {country.Population ?? 0:#,##0;;n/a}");
+			c.WriteLine($"Area: {country.Area ?? 0:#,##0.## km^2;;n/a}");
+			c.WriteLine($"Population: {country.PopulationDensity ?? 0:#,##0.## km^-2;;n/a}");
+			c.WriteLine($"Gini co-efficient: {country.Gini ?? 0:0.0;;n/a}");
+			c.WriteLine("Location: " + (!country.Location.IsUnknown ? $"{country.Location.Latitude:0.0°} {country.Location.Longitude:0.0°}" : "n/a"));
+			c.WriteLine($"Shares border(s) with: {string.Join(", ", country.SharesBorderWith.Select(c => c.NameEnglish).DefaultIfEmpty("none"))}");
+			c.WriteLine($"Language(s): {string.Join(", ", country.LanguagesCodesAlpha2.DefaultIfEmpty("none"))}");
+			c.WriteLine($"Time zone(s): {string.Join(", ", country.TimeZonesUtcOffset.Select(z => $"{z.Hours:+00;-00;00}{z:\\hmm}").DefaultIfEmpty("none"))} UTF offset");
+			c.WriteLine($"Calling code(s): {string.Join(", ", country.CallingCodes.DefaultIfEmpty("none"))}");
+			c.WriteLine($"Internet-TLD(s): {string.Join(", ", country.TopLevelDomains.DefaultIfEmpty("none"))}");
+			c.WriteLine($"Currency/ies: {string.Join(", ", country.Currencies.DefaultIfEmpty("none"))}");
 		}
 
 		private class Country {
@@ -68,9 +133,9 @@ namespace StrubT.PlayGround.CSharpConsole {
 
 			public string NameNative { get; set; }
 
-			internal ICollection<string> NamesAlternateRaw { get; set; }
+			internal ICollection<string> NamesAlternativeRaw { get; set; }
 
-			public ICollection<string> NamesAlternate => NamesAlternateRaw.Except(new[] { CodeAlpha2, CodeAlpha3, NameEnglish, NameNative }).ToList();
+			public ICollection<string> NamesAlternative => NamesAlternativeRaw.Except(new[] { CodeAlpha2, CodeAlpha3, NameEnglish, NameNative }).ToList();
 
 			internal IDictionary<string, string> NamesTranslatedRaw { get; set; }
 
@@ -90,6 +155,8 @@ namespace StrubT.PlayGround.CSharpConsole {
 
 			public double? Area { get; set; } //km^2
 
+			public double? PopulationDensity => Population / Area;
+
 			public double? Gini { get; set; }
 
 			public GeoCoordinate Location { get; set; }
@@ -102,7 +169,7 @@ namespace StrubT.PlayGround.CSharpConsole {
 
 			public ICollection<Country> SharesBorderWith => SharesBorderWithCodesAlpha3.Join(Lookup, a => a, c => c.CodeAlpha3, (a, c) => c).ToList();
 
-			public ICollection<string> SpokenLanguagesCodesAlpha2 { get; set; }
+			public ICollection<string> LanguagesCodesAlpha2 { get; set; }
 
 			public ICollection<string> TimeZonesCodes { get; set; }
 
