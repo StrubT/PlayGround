26d69e9ad7c7b11b1e3ec1001402a04685a6b5c2
Thomas Reto Strub <ths@strubt.ch>
2016-12-21 00:01:34 +0100

    add C# 7 TODO, update NuGet

---
+++ CSharpConsole/CSharp7.cs
@@ -0,0 +1,20 @@
+﻿namespace StrubT.PlayGround.CSharpConsole {
+
+	public class CSharp7 : IRunnable {
+
+		public bool Active => false;
+
+		public void Run() {
+
+			// SOURCE: https://docs.microsoft.com/en-us/dotnet/articles/csharp/csharp-7 //
+
+			//out variables
+			//Tuples
+			//Pattern Matching
+			//ref locals and returns
+			//Local Functions
+			//Generalized async return types
+			//Numeric literal syntax improvements
+		}
+	}
+}
